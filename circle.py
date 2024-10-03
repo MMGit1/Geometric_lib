979116ba6f2075c59249bbc5519ac796d306316f
MMGit1 <mi.merkushev@icloud.com>
2024-10-03 08:40:54 +0300

    Added file Documentation.md. Added comments in files.

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    # Принимает радиус круга, возвращает площадь круга.
     return math.pi * r * r
 
 
 def perimeter(r):
+    # Принимает радиус круга, возвращает периметр круга.
     return 2 * math.pi * r
 
