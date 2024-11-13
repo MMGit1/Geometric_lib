86f430289ff259252cceee4ab3c8914a404040d3
MMGit1 <mi.merkushev@icloud.com>
2024-11-13 19:14:36 +0300

    Added unittests

--- triangle.py
+++ triangle.py
@@ -1,3 +1,5 @@
+import unittest
+
 def area(a,h):
     # Принимает длину стороны треугольника и длину высоты, опущенной к этой стороне.
     # Возвращает площадь треугольника.
@@ -5,3 +7,27 @@ def area(a,h):
 def perimeter(a,b,c):
     # Принимает длины трех сторон треугольника. Возвращает периметр треугольника.
     return a+b+c    
+
+class TriangleTestCase(unittest.TestCase):
+    def test_zero_area(self):
+        self.assertRaises(Exception, lambda:area(10,0))
+        
+    def test_area(self):
+        res = area(10, 10)
+        self.assertEqual(res, 50)
+        
+    def test_negative_area(self):
+        self.assertRaises(Exception, lambda:area(-1, 10))
+        
+    def test_zero_perimeter(self):
+        self.assertRaises(Exception, lambda:perimeter(10,0,2))
+        
+    def test_negative_perimeter(self):
+        self.assertRaises(Exception, lambda:perimeter(-2,2,7))
+    
+    def test_triangle_inequality_perimeter(self):
+        self.assertRaises(Exception, lambda:perimeter(2,3,30))    
+        
+    def test_perimeter(self):
+        res = perimeter(3,4 ,5 )
+        self.assertEqual(res, 12)
